6ea8c63723fc42a73d2eef4c2342a6da24bd6932
stanom <stanom@users.noreply.github.com>
2017-10-28 22:40:50 +0200

    Create monitor_pripojenia_start.sh

---
+++ monitor_pripojenia_start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# /etc/network/if-up.d/monitor_pripojenia_start
+logdir=/var/log/pingpong
+if [ ! -d "${logdir}" ]; then
+        mkdir ${logdir}
+fi
+echo "${IFACE}: Start ping" >>${logdir}/$(date +%Y-%m-%d).log
+#ping www.google.sk | while read pong; do echo "$(date +%Y-%m-%d\ %H-%M-%S): $pong" >>${logdir}/$(date +%Y-%m-%d).log; done
+ping www.sopsr.sk | while read pong; do echo "$(date +%Y-%m-%d\ %H-%M-%S): $pong" >>${logdir}/$(date +%Y-%m-%d).log; done
